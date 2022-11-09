e406042f8fa7ac993fca9aa64cdf44fca0a2032d
priyank8141 <jainpriyank789@gmail.com>
2022-11-09 23:10:53 +0530

    feat: home page added bubble

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,14 +4,18 @@ import svgfile from '../../images/homebackgound.svg'
 import freelancerImage from '../../images/f_man.20b27179.png'
 import teaPaper from '../../images/teaPaper.png'
 import tableImage from '../../images/table.1040818b.png'
+import homeShadow from '../../images/homeShadow.png'
 
 export const Home = ({ homeDivRef, scrollToContact }) => {
     return (
         <div className={styles.home} ref={homeDivRef}>
-            <div>
+            <div className={styles.bubble}>
+                <div className={styles.bubbleGreen}></div>
                 <div className={styles.bubbleOrange}></div>
+                <div className={styles.bubblePurple}></div>
+                <div className={styles.bubblePink}></div>
+                <div className={styles.bubbleYellow}></div>
                 <div className={styles.bubbleBlue}></div>
-                <div className={styles.bubbleRed}></div>
             </div>
             <img src={svgfile} className={styles.bgImage} alt="backgroundImage" />
             <div className={styles.container}>
@@ -21,7 +25,7 @@ export const Home = ({ homeDivRef, scrollToContact }) => {
                             <h2>Hey there!
                                 <br />
                                 I'm Priyank Jain <br />
-                                <span>Freelancer</span>
+                                Independent <span>Freelancer</span>
                             </h2>
                             <h4><span>I CREATE THOUSAND OF WEBSITE</span></h4>
                             <button className={styles.hiremeBtn} onClick={scrollToContact}>Hire Me</button>
@@ -32,6 +36,7 @@ export const Home = ({ homeDivRef, scrollToContact }) => {
                             <img src={freelancerImage} className={styles.freelancerImageStyle} alt="freelancerImage" />
                             <img src={tableImage} className={styles.tableImageStyle} alt="tableImage" />
                             <img src={teaPaper} className={styles.teaPaperStyle} alt="freelancerImage" />
+                            <img src={homeShadow} className={styles.homeShadowStyle} alt="tableImage" />
                         </div>
                     </div>
                 </div>
